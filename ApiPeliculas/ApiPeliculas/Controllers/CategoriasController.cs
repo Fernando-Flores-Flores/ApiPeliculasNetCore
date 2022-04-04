1cc592b355f15103e706168d1fd48ce6509dcf32
Fernando Flores <fernando.flores.flores.3f@gmail.com>
2022-04-03 23:08:09 -0400

    Toca api pelicula

--- ApiPeliculas/ApiPeliculas/Controllers/CategoriasController.cs
+++ ApiPeliculas/ApiPeliculas/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using ApiPeliculas.Models.Dtos;
+﻿using ApiPeliculas.Models;
+using ApiPeliculas.Models.Dtos;
 using ApiPeliculas.Repository.IRepository;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
@@ -14,6 +15,7 @@ namespace ApiPeliculas.Controllers
     {
         private readonly ICategoriaRepository _ctRepo;
         private readonly IMapper _mapper;
+
         public CategoriasController(ICategoriaRepository ctRepo,IMapper mapper)
         {
             _ctRepo = ctRepo;
@@ -38,6 +40,63 @@ namespace ApiPeliculas.Controllers
             }
             var itemCategoriaDto=_mapper.Map<CategoriaDto>(itemCategoria);
             return Ok(itemCategoriaDto);
+
+        }
+
+        [HttpPost]
+        public IActionResult CrearCategoria([FromBody] CategoriaDto categoriaDto) {
+            if (categoriaDto == null) {
+                return BadRequest(ModelState);
+            }
+            if (_ctRepo.ExisteCategoria(categoriaDto.Nombre)) {
+                ModelState.AddModelError("", "La categoría ya existe");
+                return StatusCode(404, ModelState);
+            }
+            var categoria = _mapper.Map<Categoria>(categoriaDto);
+            if (!_ctRepo.CrearCategoria(categoria))
+            {
+                ModelState.AddModelError("", $"Algo salio mal guardando el registro {categoria.Nombre}");
+                return StatusCode(500, ModelState);
+            }
+            return CreatedAtRoute("GetCategoria", new { categoriaId = categoria.Id},categoria);
+        }
+
+        [HttpPatch("{categoriaId:int}", Name = "ActualizarCategoria")]
+        public IActionResult ActualizarCategoria(int categoriaId, [FromBody] CategoriaDto categoriaDto)
+        {
+            if (categoriaDto == null || categoriaId != categoriaDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+            var categoria = _mapper.Map<Categoria>(categoriaDto);
+
+            if (!_ctRepo.ActualizarCategoria(categoria))
+            {
+                ModelState.AddModelError("", $"Algo salio mal actualizando el registro {categoria.Nombre}");
+                return StatusCode(500,ModelState);
+            }
+            return NoContent();
+
+        }
+
+
+        [HttpDelete("{categoriaId:int}", Name = "BorrarCategoria")]
+        public IActionResult BorrarCategoria(int categoriaId)
+        {
+           // var categoria = _mapper.Map<Categoria>(categoriaDto);
+
+            if (!_ctRepo.ExisteCategoria(categoriaId))
+            {
+               return NotFound();
+            }
+            var categoria = _ctRepo.GetCategoria(categoriaId);
+
+            if (!_ctRepo.BorrarCategoria(categoria))
+            {
+                ModelState.AddModelError("", $"Algo salio mal borrando el registro {categoria.Nombre}");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
 
         }
 
